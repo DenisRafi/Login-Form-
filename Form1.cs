9d92c2ed0371317e2833360836aa50cffeeef6b7
Denis Rafi <62852838+DenisRafi@users.noreply.github.com>
2020-05-25 02:52:41 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Username = "DenisRafi";
+            String Pass = "123456789";
+
+            if ((textBox1.Text == Username) && (textBox2.Text == Pass))
+                MessageBox.Show("Login Successful");
+            else
+                MessageBox.Show("Enter valid Username & Password");
+        }
+    }
+}
